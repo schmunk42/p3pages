e822736380df805e360d97adb488b416ec7a8f25
Tobias Munk <schmunk@usrbin.de>
2012-03-16 20:15:02 +0100

    updated URL rule

--- config/main.php
+++ config/main.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Config file.
  *
@@ -7,12 +8,19 @@
  * @copyright Copyright &copy; 2005-2011 diemeisterei GmbH
  * @license http://www.phundament.com/license/
  */
-
 return array(
 	'modules' => array(
 		'p3pages' => array(
 			'class' => 'application.modules.p3pages.P3PagesModule',
 		)
+	),
+	'components' => array(
+		'urlManager' => array(
+			'rules' => array(
+				// p3media
+				'<lang:[a-z]{2}>/<pageName:[a-zA-Z0-9-._]*>-<pageId:\d+>.p3' => 'p3pages/default/page', // p3media images, TESTING: disable in case of problems
+			),
+		),
 	)
-)
+	)
 ?>
