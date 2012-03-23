85839c3d5718175912692185ba3bc0466c93586d
Tobias Munk <schmunk@usrbin.de>
2012-03-23 01:22:11 +0100

    refactoring; updated sitemap view

--- components/pageTree/views/tree.php
+++ components/pageTree/views/tree.php
@@ -1,12 +1,14 @@
-<h3><?php echo CHtml::link($model->t('menuName'),$model->createUrl()) ?>  <?php foreach ($model->p3PageTranslations AS $translation): ?>
-		<?php echo CHtml::link($translation->language, array('/p3pages/p3PageTranslation/update', 'id' => $translation->id), array('class' => 'btn btn-small')) ?>
-	<?php endforeach; ?></h3>
+<h3><?php echo CHtml::link($model->t('menuName'), $model->createUrl()) ?> <?php echo CHtml::link(Yii::t('app', 'Update'), array('/p3pages/p3PageMeta/update', 'id' => $model->id), array('class' => 'btn')) ?> </h3>
 <p>
-	<b><?php echo $model->p3PageMeta->treePosition ?></b>
-	<?php echo CHtml::link(Yii::t('app', 'Meta Data'), array('/p3pages/p3PageMeta/update', 'id' => $model->id), array('class' => 'btn')) ?>
+
+	<?php foreach ($model->p3PageTranslations AS $translation): ?>
+		<?php echo CHtml::link($translation->language, array('/p3pages/p3PageTranslation/update', 'id' => $translation->id), array('class' => 'btn btn-small')) ?>
+	<?php endforeach; ?>
+
 </p>
 <p>
-	<?php echo $model->layout ?>; <?php echo $model->view ?>; <?php echo $model->route ?>
+	<b>Position: <?php echo $model->p3PageMeta->treePosition ?></b>
+	Layout: <?php echo $model->layout ?> Vew: <?php echo $model->view ?> Route: <?php echo $model->route ?>
 
 	<?php echo CHtml::link(Yii::t('app', 'Update'), array('/p3pages/p3Page/update', 'id' => $model->id), array('class' => 'btn')) ?> 
-<?php echo CHtml::link(Yii::t('app', 'View '), array('/p3pages/p3Page/view', 'id' => $model->id), array('class' => 'btn')) ?></p>
+	<?php echo CHtml::link(Yii::t('app', 'Details '), array('/p3pages/p3Page/view', 'id' => $model->id), array('class' => 'btn')) ?></p>
--- config/main.php
+++ config/main.php
@@ -11,7 +11,7 @@
 return array(
 	'modules' => array(
 		'p3pages' => array(
-			'class' => 'application.modules.p3pages.P3PagesModule',
+			'class' => 'ext.p3pages.P3PagesModule',
 		)
 	),
 	'components' => array(
