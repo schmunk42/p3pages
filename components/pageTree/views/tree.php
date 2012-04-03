4fb31bfe25c3492e9c5381a91609bf1159806f9e
Tobias Munk <schmunk@usrbin.de>
2012-04-04 00:28:24 +0200

    added create button for translations

--- components/pageTree/views/tree.php
+++ components/pageTree/views/tree.php
@@ -4,7 +4,7 @@
 	<?php foreach ($model->p3PageTranslations AS $translation): ?>
 		<?php echo CHtml::link($translation->language, array('/p3pages/p3PageTranslation/update', 'id' => $translation->id), array('class' => 'btn btn-small')) ?>
 	<?php endforeach; ?>
-
+	<?php echo CHtml::link("Create New Translation", array('/p3pages/p3PageTranslation/create', 'P3PageTranslation'=>array('p3_page_id' => $model->id)), array('class' => 'btn btn-small')) ?>
 </p>
 <p>
 	<b>Position: <?php echo $model->p3PageMeta->treePosition ?></b>
